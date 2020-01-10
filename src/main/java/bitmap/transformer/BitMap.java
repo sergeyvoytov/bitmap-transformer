1281d5cc36553adb4e759366c5bccfe01d49af53
Sergey Voytov <sergvoytov@gmail.com>
2020-01-10 13:43:54 -0800

    changes

--- src/main/java/bitmap/transformer/BitMap.java
+++ src/main/java/bitmap/transformer/BitMap.java
@@ -1,10 +1,6 @@
 package bitmap.transformer;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class BitMap {
 
